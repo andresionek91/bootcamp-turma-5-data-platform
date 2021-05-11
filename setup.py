73c65b318b7479af53b0c23443091acb6612a797
Andre <andresionek91@gmail.com>
2021-05-11 23:40:18 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="bootcamp_turma_5_data_platform",
+    version="0.0.1",
+
+    description="An empty CDK Python app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="author",
+
+    package_dir={"": "bootcamp_turma_5_data_platform"},
+    packages=setuptools.find_packages(where="bootcamp_turma_5_data_platform"),
+
+    install_requires=[
+        "aws-cdk.core==1.102.0",
+    ],
+
+    python_requires=">=3.6",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+
+        "Typing :: Typed",
+    ],
+)
