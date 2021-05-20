9adbb99a2bf921103053eaebcc4c2ec0b891df23
Andre <andresionek91@gmail.com>
2021-05-20 22:59:25 +0100

    atualiza script local

--- local_scripts/update_to_rds.py
+++ local_scripts/update_to_rds.py
@@ -19,4 +19,12 @@ print("connected")
 conn.set_session(autocommit=True)
 cur = conn.cursor()
 
-cur.execute(f"update orders_agora_vai set value = 0.5 where order_id=988")
+cur.execute(
+    f"update orders_v2 set value = 500000 where order_id='86a9a8a0-682e-435d-955e-dd61d888412b'"
+)
+cur.execute(
+    f"update orders_v2 set value = 200 where order_id='86a9a8a0-682e-435d-955e-dd61d888412b'"
+)
+cur.execute(
+    f"delete from orders_v2 where order_id='87744bf8-05b4-4e51-807e-5a07960fb450'"
+)
